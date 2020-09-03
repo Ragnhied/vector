88f8943010092f8c29d472f4092d6a768b160022
Ragnhild Elise Dirdal <ragnhilddirdal@eduroam-193-157-213-78.wlan.uio.no>
2020-09-03 11:04:24 +0200

    Implementerte add og eq laget testfunksjoner

--- test_vector1.py
+++ test_vector1.py
@@ -7,5 +7,19 @@ def test_vector_str(): #sjekke at koden printer det vi vil den skal printe
     assert str(v) == "(1,2,3)" #str(v) printer string. Dette vi gjorde med __str__
 
 def test_vector_repr():
-     v = Vector3D(1,2,3)
+     v = Vector3D(1, 2, 3)
      assert repr(v) == "Vector3D(1,2,3)"
+
+def test_vector_add():
+    v = Vector3D(1, 2, 3)
+    u = Vector3D(1, 1, 1)
+    w = u + v
+    assert w.x == 2
+    assert w.y == 3
+    assert w.z == 4
+
+def test_vector_add_eq():
+    v = Vector3D(1, 2, 3)
+    u = Vector3D(1, 1, 1)
+    w = u + v
+    assert w == Vector3D(2, 3, 4) #sjekker om en vektor er lik en annen
--- vector1.py
+++ vector1.py
@@ -13,7 +13,26 @@ class Vector3D:
     
     def __repr__(self):
         return f"{self.__class__.__name__}({self.x},{self.y},{self.z})" #self.class.name = får tak i navnet på klassen
+
+    def __add__(self,other): #må ikke kalle for self og other, men alle andre gjør det så er lurt.
+        return Vector3D(self.x + other.x, self.y + other.y, self.z + other.z)
+    
+    def __eq__(self, other): #metoden returnerer True hvis self == other
+        return self.x == other.x and self.y == other.y and self.z == other.z
+
+    
+
+"""
+Forskjell på str og repr: repr bør være litt mer presis enn str-metoden. 
+Huskeregel er at x == eval(repr(x)). Den evaluerer en streng som egentlig er kode
+Når man implementerer klasser bør man alltid implementere repr, fint når man skal debugge.
+Om man ikke implementerer str-metoden, går den automatisk over på repr. 
+"""
+
     
 if __name__ == "__main__":
-    v = Vector3D(1, 1, 1)
-    print([v])    
+    v = Vector3D(1, 4, 2)
+    u = Vector3D(1, 1, 1)
+    w = v + u
+    w == Vector3D(2, 5, 3)
+     
