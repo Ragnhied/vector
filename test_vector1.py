c87287c12818d101cc4f39608ce7e0fd14313049
Ragnhild Elise Dirdal <ragnhilddirdal@eduroam-193-157-213-78.wlan.uio.no>
2020-09-04 14:07:46 +0200

    Implementerte kryssproduktet og dotproduktet.

--- test_vector1.py
+++ test_vector1.py
@@ -2,13 +2,15 @@ import pytest
 from vector1 import Vector3D
 
 
-def test_vector_str(): #sjekke at koden printer det vi vil den skal printe
-    v = Vector3D(1,2,3)
-    assert str(v) == "(1,2,3)" #str(v) printer string. Dette vi gjorde med __str__
+def test_vector_str():  # sjekke at koden printer det vi vil den skal printe
+    v = Vector3D(1, 2, 3)
+    assert str(v) == "(1,2,3)"  # str(v) printer string. Dette vi gjorde med __str__
+
 
 def test_vector_repr():
-     v = Vector3D(1, 2, 3)
-     assert repr(v) == "Vector3D(1,2,3)"
+    v = Vector3D(1, 2, 3)
+    assert repr(v) == "Vector3D(1,2,3)"
+
 
 def test_vector_add():
     v = Vector3D(1, 2, 3)
@@ -18,17 +20,22 @@ def test_vector_add():
     assert w.y == 3
     assert w.z == 4
 
+
 def test_vector_add_eq():
     v = Vector3D(1, 2, 3)
     u = Vector3D(1, 1, 1)
     w = u + v
-    assert w == Vector3D(2, 3, 4) #sjekker om en vektor er lik en annen
+    assert w == Vector3D(2, 3, 4)  # sjekker om en vektor er lik en annen
+
 
 def test_vector_add_integer():
     v = Vector3D(1, 2, 3)
     u = 1
-    w = v + u #Måtte skrive  v+u (ikke u+v) for at den skulle funke. Neste testfunksjon viser u+v
+    w = (
+        v + u
+    )  # Måtte skrive  v+u (ikke u+v) for at den skulle funke. Neste testfunksjon viser u+v
     assert w == Vector3D(2, 3, 4)
+
 
 def test_vector_add_integer_right():
     v = Vector3D(1, 2, 3)
@@ -36,16 +43,60 @@ def test_vector_add_integer_right():
     w = u + v
     assert w == Vector3D(2, 3, 4)
 
+
 def test_vector_add_string_raises_TypeError():
     v = Vector3D(1, 2, 3)
     u = "Hello"
     with pytest.raises(TypeError):
-        u + v 
-    
+        u + v
+
+
 def test_vector_dot_product():
     v = Vector3D(1, 2, 3)
     u = Vector3D(1, 1, 1)
     w = u.dot(v)
-    assert isinstance(w, (int,float)) #sjekker om w enten ble en int eller float
+    assert isinstance(w, (int, float))  # sjekker om w enten ble en int eller float
     assert abs(w - 6) < 1e-12
-    #assert math.isclose(w,6) #Alternativ metode til 1e-12. Da må man importere math først!
+    # assert math.isclose(w,6) #Alternativ metode til 1e-12. Da må man importere math først!
+
+
+def test_vector_dot_product_mul():
+    v = Vector3D(1, 2, 3)
+    u = Vector3D(1, 1, 1)
+    w = u * v
+    assert isinstance(w, (int, float))
+    assert abs(w - 6) < 1e-12
+
+
+@pytest.mark.parametrize(
+    "u, v, expected",
+    [
+        (Vector3D(0, 1, 0), Vector3D(1, 0, 0), Vector3D(0, 0, -1)),
+        (Vector3D(2, 0, -2), Vector3D(2, 4, 2), Vector3D(8, -8, 8)),
+    ],
+)
+def test_vector_cross_product(u, v, expected):
+    # v = Vector3D(1, 0, 0)
+    # u = Vector3D(0, 1, 0)
+    w = u.cross(v)
+    assert w == expected  # Vector3D(0, 0, -1)
+
+
+def test_vector_cross_product2():
+    u = Vector3D(2, 4, 2)
+    v = Vector3D(2, 0, -2)
+    w = u.cross(v)
+    assert w == Vector3D(-8, 8, -8)
+
+
+# @pytest.mark.parametrize(
+#     "u, v, expected",
+#     [
+#         (Vector3D(0, 1, 0), Vector3D(1, 0, 0), Vector3D(0, 0, -1)),
+#         (Vector3D(2, 0, -2), Vector3D(2, 4, 2), Vector3D(8, -8, 8)),
+#     ],
+# )
+# def test_vector_cross_product_matmul(u, v, expected):
+#     """Multipliserer matriser"""
+#     w = u @ v
+#     assert w == expected
--- vector1.py
+++ vector1.py
@@ -1,38 +1,78 @@
-#01.09.20
-#fortsettelse på objekt-orientert programmering
-#KLASSER I PYTHON
+# 01.09.20
+# fortsettelse på objekt-orientert programmering
+# KLASSER I PYTHON
+
 
 class Vector3D:
-    def __init__(self,x,y,z): #init er en konstruktør
-        self.x = x #Legger til som atributter på klassen
+    def __init__(self, x, y, z):  # init er en konstruktør
+        self.x = x  # Legger til som atributter på klassen
         self.y = y
         self.z = z
-    
-    def __str__(self): #for å få finere print. 
+
+    def __str__(self):  # for å få finere print.
         return f"({self.x},{self.y},{self.z})"
-    
+
     def __repr__(self):
-        return f"{self.__class__.__name__}({self.x},{self.y},{self.z})" #self.class.name = får tak i navnet på klassen
+        return f"{self.__class__.__name__}({self.x},{self.y},{self.z})"  # self.class.name = får tak i navnet på klassen
 
-    def __radd__(self,other): #rightadd, adder fra andre siden
-        return self.__add__(other) #denne vil kun brukes hvis __add__ ikke klarer legge til på vanlig måte. 
+    def __radd__(self, other):  # rightadd, adder fra andre siden
+        return self.__add__(
+            other
+        )  # denne vil kun brukes hvis __add__ ikke klarer legge til på vanlig måte.
 
-
-    def __add__(self,other): #må ikke kalle for self og other, men alle andre gjør det så er lurt. Det finnes en helt lik metode: __sub__ som trekker fra. 
-        if  isinstance(other, (int,float)):
+    def __add__(
+        self, other
+    ):  # må ikke kalle for self og other, men alle andre gjør det så er lurt. Det finnes en helt lik metode: __sub__ som trekker fra.
+        if isinstance(other, (int, float)):
             return Vector3D(self.x + other, self.y + other, self.z + other)
         elif isinstance(other, Vector3D):
             return Vector3D(self.x + other.x, self.y + other.y, self.z + other.z)
-        else: 
+        else:
             raise TypeError(f"Cannot add Vector3D to object of type {type(other)}")
 
-    def __eq__(self, other): #metoden returnerer True hvis self == other
+    def __eq__(self, other):  # metoden returnerer True hvis self == other
         return self.x == other.x and self.y == other.y and self.z == other.z
-    
-    def dot(self,other):
-        return self.x * other.x + self.y * other.y + self.z * other.z #produkt
 
-    
+    def dot(self, other):
+        """Take the dot product between this vector and another vector
+
+        Arguments
+        -----------
+        other : Vector3d
+            The vector to be dotted with
+
+        Returns
+        ----------
+        float
+            The dot product
+        """
+        return self.x * other.x + self.y * other.y + self.z * other.z  # produkt
+
+    def __mul__(self, other):
+        return self.dot(other)  # for å kunne skrive u*v
+
+    def cross(self, other):
+        """Take the cross product between this vector (self) and another vector
+
+        Arguments
+        -----------
+        other : Vector3D
+            The vector to be crossed with
+
+        Returns
+        ----------
+        Vector3D
+            The cross product
+        """
+        return Vector3D(
+            self.y * other.z - self.z * other.y,
+            self.z * other.x - self.x * other.z,
+            self.x * other.y - self.y * other.x,
+        )
+
+        # def __matmul__(self, other):
+        #     return self.cross(other)
+
 
 """
 Forskjell på str og repr: repr bør være litt mer presis enn str-metoden. 
@@ -41,10 +81,9 @@ Når man implementerer klasser bør man alltid implementere repr, fint når man 
 Om man ikke implementerer str-metoden, går den automatisk over på repr. 
 """
 
-    
+
 if __name__ == "__main__":
     v = Vector3D(1, 4, 2)
     u = Vector3D(1, 1, 1)
     w = v + u
     w == Vector3D(2, 5, 3)
-     
